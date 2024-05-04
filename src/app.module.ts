2b0299840422cb49c170eb291ec17a37b3a8569e
wojciech <wmankowski@gmail.com>
2024-05-04 18:38:32 +0200

    add Middleware

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,15 @@
-import { Module } from '@nestjs/common';
+import { MiddlewareConsumer, Module, NestModule } from '@nestjs/common';
 import { ApiModule } from './api/api.module';
 import { AppService } from './app.service';
-
+import { LoggerMiddleware } from './common/logger/logger.middleware';
 
 @Module({
   imports: [ApiModule],
   controllers: [],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule implements NestModule {
+  configure(consumer: MiddlewareConsumer) {
+    consumer.apply(LoggerMiddleware).forRoutes('*');
+  }
+}
